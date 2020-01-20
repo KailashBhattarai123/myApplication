c97cb0e68ad8fe2509f09b4b14ea59a62135f637
Kshitiz <Kshitizbhandari23@gmail.com>
2020-01-20 12:48:45 +0545

    Branch

---
+++ app/src/main/java/com/horizon/helloapplication/Ads.java
@@ -0,0 +1,10 @@
+package com.horizon.helloapplication;
+
+public class Ads {
+
+    public String Messa() {
+        return "Hii Nested Mango.";
+        //feature..ads..ui..element....working with team.
+
+    }
+}
